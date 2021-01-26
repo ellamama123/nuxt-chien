63ce716eb0c832b504a042cddb3cd71ab662b43d
ellamama123 <67947014+ellamama123@users.noreply.github.com>
2021-01-26 22:57:16 +0700

    Update AppMenu.vue

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -1,16 +1,16 @@
 <template lang="">
     <ul class="nav flex-column">
         <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
+            <a class="nav-link active" href="/">List</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="/add">New</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="/search">Search</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Disabled</a>
+            <a class="nav-link" href="/edit">Edit</a>
         </li>
     </ul>  
 </template>
